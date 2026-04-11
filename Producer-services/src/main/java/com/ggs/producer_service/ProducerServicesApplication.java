c9c31ce682d1bdce40e9628a4627656cbc0902b1
Gopi-o <Aleshi.kriv@Gmail.com>
2026-04-11 11:04:37 +0400

    Init Producer & Consumer Services

---
+++ Producer-services/src/main/java/com/ggs/producer_service/ProducerServicesApplication.java
@@ -0,0 +1,13 @@
+package com.ggs.producer_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerServicesApplication.class, args);
+	}
+
+}
---
+++ Producer-services/src/test/java/com/ggs/producer_service/ProducerServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggs.producer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
